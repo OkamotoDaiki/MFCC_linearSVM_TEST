58a1bee58b5cc20f6387c01940a8e8d6884b4d07
Oka_D <okamotoschool2018@gmail.com>
2022-06-21 20:33:11 +0900

    add normalization code.

--- linearSVM.py
+++ linearSVM.py
@@ -2,9 +2,9 @@ import glob
 import soundfile
 from subscript import FFTTool, MFCC
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score
 from sklearn.svm import LinearSVC
-
 
 if __name__=="__main__":
     fpath = "recordings/*.wav"
@@ -20,12 +20,12 @@ if __name__=="__main__":
         cutpoint = 12
         mfcc = MFCC.MFCCclass(window_data , fs, cutpoint=12, numChannels=20)
         mfcc_array = mfcc.MFCC()
+        mfcc_array = preprocessing.scale(mfcc_array) #normalization
         training_list.append(mfcc_array)
         label = fname.split('/')[1].split('_')[0]
         label_list.append(label)
 
-    from sklearn.model_selection import cross_val_score
-
+    """linear SVM"""
     linear_svm = LinearSVC()
     scores = cross_val_score(linear_svm, training_list, label_list, cv=5)
     print("Cross-validation scores: {:.3f}".format(scores.mean()))
